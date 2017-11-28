3e6fe8a98caf89217d7b23bc1d08be7a50f4cb1c
hermansantoso <32539617+hermansantoso@users.noreply.github.com>
2017-11-28 13:59:07 +0800

    Create backend_response.php

---
+++ sample/backend_response.php
@@ -0,0 +1,19 @@
+<?php
+
+require "enets2.php";
+$enets = new Enets2;
+
+$file = fopen("log.txt","a+");
+fwrite($file,date("Y-m-d H:i:s".": "));
+date_default_timezone_set("Asia/Singapore");
+try {
+    $enets->setSecretKey("a7002075-a716-47b9-948a-43927cf4c634");
+    $response = $enets->getBackendResponse();
+    fwrite($file,print_r($response,true).PHP_EOL);
+    fwrite($file,print_r($enets->getNetsMessage(),true).PHP_EOL);
+} catch (Exception $e) {
+    fwrite($file,$e->getMessage().PHP_EOL);
+}
+fclose($file);
+
+?>
